641a59c94207fdaf184aab4f5db3674cab3264da
Your Name <you@example.com>
2025-08-16 02:03:06 +0330

    ok 6

--- api/Program.cs
+++ api/Program.cs
@@ -36,7 +36,7 @@ app.MapGet("/weatherforecast", () =>
 .WithName("GetWeatherForecast")
 .WithOpenApi();
 
-app.MapGet("/health", () => Results.Ok("OK5")).WithName("Health");
+app.MapGet("/health", () => Results.Ok("OK6")).WithName("Health");
 
 
 app.Run();
